e2ea315d3c3347843b6e2497f50e104e08ed9be8
interfame <interfamemediagroup@gmail.com>
2026-01-10 22:33:29 -0300

    Update header.js

--- header.js
+++ header.js
@@ -1,31 +1,3 @@
-alert("HEADER JS EJECUTÁNDOSE");
-
-import { supabase } from "./supabaseClient.js";
-
-console.log("HEADER JS CARGADO");
-
-async function loadMenu() {
-  const { data, error } = await supabase
-    .from("menu_categories")
-    .select("*")
-    .order("position");
-
-  console.log("DATA:", data);
-  console.log("ERROR:", error);
-
-  const ul = document.getElementById("dynamic-menu");
-  if (!ul) return;
-
-  ul.innerHTML = "";
-
-  data.forEach(cat => {
-    const li = document.createElement("li");
-    li.className = "nav-item";
-    li.innerHTML = `<a class="nav-link">${cat.title}</a>`;
-    ul.appendChild(li);
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const ul = document.getElementById("dynamic-menu");
 
@@ -37,7 +9,3 @@ document.addEventListener("DOMContentLoaded", () => {
     </li>
   `;
 });
-
-
-loadMenu();
-
