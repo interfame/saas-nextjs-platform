ab7173f70d89f808ccd4f6efca276f35695e88ae
interfame <interfamemediagroup@gmail.com>
2026-01-10 22:15:35 -0300

    Update header.js

--- header.js
+++ header.js
@@ -26,5 +26,18 @@ async function loadMenu() {
   });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const ul = document.getElementById("dynamic-menu");
+
+  ul.innerHTML = `
+    <li class="nav-item">
+      <a class="nav-link" style="color:blue;font-weight:bold">
+        🔵 HEADER.JS FUNCIONA
+      </a>
+    </li>
+  `;
+});
+
+
 loadMenu();
 
