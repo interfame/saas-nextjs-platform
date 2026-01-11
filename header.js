4cd55f2b59edd8cbff9ab4ad84a096eff3601f93
interfame <interfamemediagroup@gmail.com>
2026-01-10 22:48:21 -0300

    Update header.js

--- header.js
+++ header.js
@@ -1,11 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const ul = document.getElementById("dynamic-menu");
+const supabase = window.supabase.createClient(
+  "https://jwfesxzzbehzeytwcbgz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3ZmVzeHp6YmVoemV5dHdjYmd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5Mjg2NDMsImV4cCI6MjA4MzUwNDY0M30.SLoar0LgTTOlkB2gVlJ-IU9YBh0uygqKVb1pBpKtBWo"
+);
 
-  ul.innerHTML = `
-    <li class="nav-item">
-      <a class="nav-link" style="color:blue;font-weight:bold">
-        🔵 HEADER.JS FUNCIONA
-      </a>
-    </li>
-  `;
-});
+async function loadMenu() {
+  const { data, error } = await supabase
+    .from("menu_categories")
+    .select("title")
+    .order("position");
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const ul = document.getElementById("dynamic-menu");
+  ul.innerHTML = "";
+
+  data.forEach(cat => {
+    ul.innerHTML += `
+      <li class="nav-item">
+        <a class="nav-link">${cat.title}</a>
+      </li>
+    `;
+  });
+}
+
+loadMenu();
